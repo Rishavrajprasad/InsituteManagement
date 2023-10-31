5a35a2b9acac156e4c84f53042eaa41e665ae196
Rishavrajprasad <rishavrajprasad800@gmail.com>
2023-10-31 19:29:55 +0530

    create user model

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import bgImage from '../assets/images/bg.jpg'; 
 import logo from '../assets/images/bit.png'
 const bg= 'https://www.bitmesra.ac.in/UploadedDocuments/user_admindeoghar/Header/Header4852371f5c88441dab8c58e3bf26d9bb_College%20Photo%202.jpg'
 
 export default function Home() {
- 
+  const [formData, setFormData] = useState({});
+  const handleChange =(e)=>{
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  console.log(formData);
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+  }
   return (
     <div
       className="bg-cover bg-center h-screen"
@@ -19,9 +29,9 @@ export default function Home() {
       <h1 className='font-montserrat text-sm sm:text-xl text-white tracking-widest'>BIRLA INSTITUTE OF TECHNOLOGY, DEOGHAR</h1>
       <div className='p-3 max-w-lg mx-auto'>
         <p className='font-montserrat  text-white tracking-widest text-shadow text-sm sm:text-lg my-5 mx-7'>Welcome, Please Log in to Continue!</p>
-        <form className='flex flex-col gap-4' >
-          <input type="email" placeholder='Username' className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none' id='email'/>
-          <input type="password" placeholder='Password' className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none' id='password'/>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4' >
+          <input type="email" placeholder='Username' className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none' id='email'  onChange={handleChange}/>
+          <input type="password" placeholder='Password' className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none' id='password' onChange={handleChange}/>
           <button className='bg-gradient-to-r from-blue-500 to-purple-500 text-white font-semibold py-2 px-4 rounded-md  hover:opacity-90'>Sign in</button>
         </form>
       </div>
