24c83742055c27c2ce3e1de2da9c6c2a70a07a5b
Rishavrajprasad <rishavrajprasad800@gmail.com>
2023-11-07 22:04:34 +0530

    API Updated

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -34,6 +34,6 @@ const listingSchema = new mongoose.Schema(
     },{timestamps : true}
 )
 
-const Listing = mongoose.model('Listing', listingSchema);
+const Listing = mongoose.model('Faculty_salary', listingSchema);
 
 export default Listing;
