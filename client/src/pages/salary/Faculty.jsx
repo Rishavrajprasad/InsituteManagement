da999ec72624cf34153c2c05f32c02dda0c346c0
Rishavrajprasad <rishavrajprasad800@gmail.com>
2023-11-07 21:56:34 +0530

    Faculty Page Completed

--- client/src/pages/salary/Faculty.jsx
+++ client/src/pages/salary/Faculty.jsx
@@ -1,9 +1,71 @@
 import React from 'react';
-
+import { useState } from 'react';
 import bgImage from '../../assets/images/bg.jpg';
 import Header from '../../components/Header';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Faculty() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    
+    fname: '',
+    email: '',
+    phone: '',
+    branchselect: '',
+    postselect: '',
+    salary: 0,
+    month: '',
+  });
+  console.log(formData);
+  const handleChange =(e)=>{
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+    if (
+      e.target.type === 'number' ||
+      e.target.type === 'text' ||
+      e.target.type === 'textarea'
+    ) {
+      setFormData({
+        ...formData,
+        [e.target.id]: e.target.value,
+      });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/listing/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...formData,
+          userRef: currentUser._id,
+        }),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(data.message);
+      }
+      navigate(`/listing/${data._id}`);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
         <div>
@@ -13,32 +75,42 @@ export default function Faculty() {
         <div
         className="bg-cover bg-center h-screen"
         style={{
-          backgroundImage: `url(${bgImage})`, // Use the imported image
+          backgroundImage: `url(${bgImage})`, 
         }}
       >
         <div className="text-white text-md md:text-3xl uppercase font-bold text-center py-5">
           Faculty Salary
         </div>
         <div className='p-3 max-w-lg mx-auto'>
-            <form className='flex flex-col gap-4' >
-          <input type="text" placeholder='Full Name' className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none' id='fname'required/>
-          <input type="email" placeholder='Email' className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none' id='email'required/>
-          <input type="tel" placeholder='Phone Number' className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none' id='phone'required/>
-          <select id="branchselect" className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none'>
+            <form onSubmit={handleSubmit} className='flex flex-col gap-4' >
+          <input type="text" placeholder='Full Name' className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none' id='fname'onChange={handleChange}
+            value={formData.fname}
+            required/>
+          <input type="email" placeholder='Email' className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none' id='email' onChange={handleChange}
+            value={formData.email}
+            required/>
+          <input type="tel" placeholder='Phone Number' className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none' id='phone' onChange={handleChange}
+            value={formData.phone} required/>
+          <select id="branchselect" type='text' className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none' onChange={handleChange}
+            value={formData.branchselect} required>
                 <option value="" disabled selected>Select Branch</option>
                 <option value="cse" id='cse'>Computer Science and Engineering</option>
                 <option value="ece" id ='ece'>Electronics & Communication Engineering</option>
                 <option value="eee" id ='eee'>Electrical and Electronics Engineering</option>
                 <option value="me" id ='me'>Mechanical Engineering</option>
         </select>
-        <select id="postselect" className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none'>
+        <select id="postselect" type='text' className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none' onChange={handleChange}
+            value={formData.postselect} required>
                 <option value="" disabled selected>Select Position</option>
                 <option value="hod" id ='hod'>Head of Department</option>
                 <option value="aprof" id ='aprof'>Associate Professor</option>
                 <option value="prof" id = 'prof'>Professor</option>
         </select>
-        <input type="text" placeholder='Salary Amount' className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none' id='salary'required/>
-        <select id="month" className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none'>
+        <input type="text" placeholder='Salary Amount' className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none' id='salary'required
+        onChange={handleChange}
+        value={formData.salary}/>
+        <select id="month" className='border p-1 px-5 rounded-lg opacity-90 focus:outline-none' onChange={handleChange}
+            value={formData.month} required>
                 
                 <option value="" disabled selected>Select Month</option>
                 <option value="January">January</option>
@@ -55,9 +127,11 @@ export default function Faculty() {
                 <option value="December">December</option>
 
         </select>
-        </form>
-        <button className='w-full bg-green-700  text-white rounded-lg p-3 my-3 uppercase hover:opacity-95 disabled:opacity-80'>Add to Database</button>
+        <button disabled={loading} className='w-full bg-green-700  text-white rounded-lg p-3 my-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Adding...' : 'Add to Database'}</button>
         <button className='w-full bg-slate-700  text-white rounded-lg p-3  uppercase hover:opacity-95 disabled:opacity-80'>Update Details</button>
+        {error && <p className='text-red-700 text-sm'>{error}</p>}
+        </form>
+        
         </div>
         
 
