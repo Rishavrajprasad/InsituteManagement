90a6d916021c3957afaa09939adaa7f99eca67d5
Rishavrajprasad <rishavrajprasad800@gmail.com>
2023-11-07 15:27:08 +0530

    Update Profile and logout

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema ({
     password : {
         type:String,
         required: true,
+    },
+    fullname : {
+        type: String,
+        default:"Profile",
     }
 }, {timestamps: true});
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,30 @@
 import logo from '../assets/images/bit.png';
-import user from '../assets/images/user.png'
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
+import { useDispatch } from 'react-redux';
+import {
+  deleteUserFailure,
+  deleteUserSuccess,
+  signOutUserStart,
+} from '../redux/user/userSlice';
 export default function Header() {
+    const dispatch = useDispatch();
     const { currentUser } = useSelector((state) => state.user);
+    const handleSignOut = async () => {
+
+      try {
+        dispatch(signOutUserStart())
+        const res = await fetch('/api/auth/signout');
+        const data = await res.json();
+        if (data.success === false) {
+          dispatch(deleteUserFailure(data.message));
+          return;
+        }
+        dispatch(deleteUserSuccess(data));
+      } catch (error) {
+        dispatch(deleteUserFailure(data.message));
+      }
+    }
   return (
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-8xl mx-auto p-3 px-10'>
@@ -29,14 +49,12 @@ export default function Header() {
           </Link>
           <Link to='/profile'>
             {currentUser ? (
-              <img className='rounded-full h-7 w-7 object-cover' src={user} alt='profile' />
+              <p className=' text-slate-700 text-lg hover:underline'>{currentUser.fullname}</p>
             ) : (
               <li className=' text-slate-700 hover:underline'> Profile</li>
             )}
           </Link>
-          <Link to='/sign-in'>
-            <li className='text-slate-700 text-lg hover:underline'>Sign out</li>
-          </Link>
+          <span onClick={handleSignOut} className='text-slate-700 text-lg hover:underline'>Sign out</span>
         </ul>
       </div>
     </header>
